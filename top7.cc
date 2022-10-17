0475edb411a308cae86b18c1537f4910e8b20f9e
michalMaszkowski <michalmaszkowski@gmail.com>
2022-10-17 17:48:31 +0200

    ujednolicenie języka na polski

--- top7.cc
+++ top7.cc
@@ -4,22 +4,24 @@
 #include <iostream>
 #include <string>
 #include <cstdint>
+#include <vector>
 #include <set>
 #include <unordered_set>
 #include <unordered_map>
 
-enum rodzaj_linii { ERROR, NEW_MAX, TOP, VOTES };
+
+enum rodzaj_linii { BLEDNA, NEW, TOP, GLOS };
 using numer_piosenki = int32_t;
 using liczba_glosow = int;
 using liczba_punktow = int;
 
-rodzaj_linii input_check_type (std::string input_line) {
+rodzaj_linii rozpoznanie_wejscia (std::string linia_wejscia) {
     /**
      *
      */
 }
 
-void linia_bledu(const std::string& input_line, int line_number) {
+void linia_bledu(const std::string& linia_wejscia, const int numer_linii) {
     std::cerr << "Error in line" << line_number << ": " << input_line;
 }
 
@@ -28,23 +30,28 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie();
 
-    std::set<song_number> top7_notowanie;
-    std::set<song_number> top7_podsumowanie;
-    std::unordered_set<song_number> wypadniete;
-    std::unordered_map<song_number> wyniki_notowania
+    std::set<numer_piosenki> top7_notowanie;
+    std::set<numer_piosenki> top7_podsumowanie;
+    std::unordered_set<numer_piosenki> wypadniete;
+    std::unordered_map<numer_piosenki> wyniki_notowania
 
 
 
 
-    int line_number;
-    std::string input_line;
-    getline(std::cin, input_line);
+    int numer_linii = 1;
+    std::string linia_wejscia;
+    getline(std::cin, linia_wejscia);
 
-    switch (input_check_type(input_line)) {
-        case ERROR: error_line(input_line, line_number); break;
-        case NEW_MAX:
-            case TOP:
-                case VOTES:
+    switch (rozpoznanie_wejscia(linia_wejscia)) {
+        case BLEDNA:
+            linia_bledu(linia_wejscia, numer_linii);
+            break;
+        case NEW:
+            break;
+        case TOP:
+            break;
+        case GLOS:
+            break;
     }
     return 0;
 }
